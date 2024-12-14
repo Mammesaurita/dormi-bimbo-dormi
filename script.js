f75f62277effaf515eba48dc59955c05f34097bd
Mammesaurita <michaeljohndidio@gmail.com>
2024-12-14 10:29:08 +0100

    caricati file principali sito

---
+++ script.js
@@ -0,0 +1,7 @@
+function scegliSuono() {
+    alert("Questa funzione sarà implementata presto!");
+}
+
+function registra() {
+    alert("Funzionalità di registrazione non disponibile al momento!");
+}
